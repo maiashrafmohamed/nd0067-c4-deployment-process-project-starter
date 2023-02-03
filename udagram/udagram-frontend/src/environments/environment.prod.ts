63df4ead986bfc7682d8d41c9b664384e351c807
maiashrafmohamed <mai.ashraf.mohamed@gmail.com>
2023-02-04 01:22:16 +0200

    update deploy frontend

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udagram-api-dev.eba-g9ztvixj.us-east-1.elasticbeanstalk.com/',
 };
 
 /*
